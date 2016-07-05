92c6ee84e195522d9f9150a7dbf9a16768ce0bf7
AnaMamic <amamic@pmfst.hr>
2016-07-05 21:58:23 +0200

    Currency Rate App

---
+++ TecajnaLista/Scripts/Application/AngularApp.js
@@ -0,0 +1,32 @@
+﻿var app = angular.module("CustomAngularApp", []);
+
+app.controller("Currencies", function ($scope, $http) {
+    $scope.show = false;
+    $http.get('http://app.novi-is.com/TestWebApi/test/currencies')
+        .then(function (response) {
+            $scope.currencies = response.data;
+            $scope.show = true;
+        }, function(response)
+        {
+            alert(response.status + " " + response.statusText);
+        });
+
+
+    $scope.getRate = function ()
+    {
+        if($scope.Currency == null)
+        {
+            alert("Odaberite valutu");
+        }
+        else
+        {
+            $http.get('http://app.novi-is.com/TestWebApi/test/rate/'+$scope.Currency.Code)
+            .then(function(response){
+                $scope.rate = response.data.CurrencyCode+" tečaj je "+ response.data.Rate;
+            }, function (response) {
+                alert(response.status + " " + response.statusText);
+            } );
+        }
+    }
+
+   });
